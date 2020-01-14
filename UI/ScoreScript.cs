6aed5392ff73d1c1b30aaec4b51dbf1051f7a75b
shumaikunkun <s1711520@s.tsukuba.ac.jp>
2020-01-15 03:34:58 +0900

    score部分のコード一般化

--- UI/ScoreScript.cs
+++ UI/ScoreScript.cs
@@ -14,18 +14,30 @@ public class ScoreScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    Debug.Log(PlayerPrefs.GetInt("Data"));
-
         playerScoreText.text="Your Score is "+PlayerDamage.playerScore;
 
-    PlayerPrefs.SetInt("Data",PlayerDamage.playerScore);
+        const int N=10; //表示するスコアの数（偶数がいい）
 
-        int[] arr = new int[10];
-        
+        int[] arr = new int[N];
+        // execute for the first time
+        // for(int i=0;i<10;i++){ arr[i]=0; }
+        // for(int i=0;i<10;i++){ PlayerPrefs.SetInt("Data"+i.ToString(),arr[i]); }  //store to allHighScore
+
+        for(int i=0;i<N;i++){ arr[i]=PlayerPrefs.GetInt("Data"+i.ToString()); }  //load from allHighScore
+
+        int m=N;  //今回のスコアが入るインデックスをメモする変数
+        for(int i=0;i<N;i++){ if(arr[N-1-i]<PlayerDamage.playerScore){ m=N-1-i; }}  //mを求める
+        if(m<N){
+            if(m<N-1){ for(int i=N-1;i>m;i--){ arr[i]=arr[i-1]; }}  //m以降順番をひとつずつずらす
+            arr[m]=PlayerDamage.playerScore;
+        }
+
+        for(int i=0;i<N;i++){ PlayerPrefs.SetInt("Data"+i.ToString(),arr[i]); }  //store to allHighScore
+
         left="";
         right="";
-        for(int i=0;i<10;i++){
-            if(i<5){
+        for(int i=0;i<N;i++){
+            if(i<N/2){
                 left+=(i+1).ToString()+"  "+arr[i]+"pt\n";
             }else{
                 right+=(i+1).ToString()+"  "+arr[i]+"pt\n";
