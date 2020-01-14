c618ae13c741a9570c6e456a112ec87fbfaf8a68
shumaikunkun <s1711520@s.tsukuba.ac.jp>
2020-01-14 20:22:56 +0900

    make private varible public one

--- Player/PlayerDamage.cs
+++ Player/PlayerDamage.cs
@@ -5,27 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDamage : MonoBehaviour
 {
-
-
     public int playerHP;
     public Text playerHPText;
 
-    public int playerScore;
+    int playerScore;
     public Text playerScoreText;
 
     public GameObject bomb;
 
-    public AudioClip sound1;  //Damage sound
     AudioSource audioSource;
+    public AudioClip sound1;  //Damage sound
+    public AudioClip sound2;  //Get Coin sound
 
-    public GameObject plane;  //to erase movie when I die
-    
     private void Start()
     {
-        playerHP =2;
+        //playerHP = 2;
         playerScore = 0;     
         audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.clip = sound1;
+        //audioSource.clip = sound1;
     }
 
     private void Update()
@@ -34,21 +31,18 @@ public class PlayerDamage : MonoBehaviour
         playerScoreText.text="Score:"+playerScore.ToString();
     }
 
-
     //　コライダのIsTriggerのチェックを入れ物理的な衝突をしない（突き抜ける）場合
     void OnTriggerEnter(Collider col) {
         //Debug.Log(col.gameObject.tag);
 		if(col.tag == "Enemy") {
             Instantiate(bomb, col.gameObject.transform.position, col.gameObject.transform.rotation);
             playerHP -= 1;
-            //playerHPText.text = "残りHP:" + playerHP.ToString();
-            // audioSource.PlayOneShot(sound1);
-            audioSource.Play();
+            audioSource.PlayOneShot(sound1);
+            //audioSource.Play();
             if (playerHP == 0)
             {
                 AudioSource.PlayClipAtPoint(sound1,Camera.main.transform.position);
                 Destroy(this.gameObject);  //Erase Player
-                //Destroy(plane.gameObject);
                 this.gameObject.SetActive(false);
                 SceneManager.LoadScene("GameOver");
 
@@ -59,6 +53,7 @@ public class PlayerDamage : MonoBehaviour
         
         if(col.tag == "Coin"){
             //Debug.Log("you get coin");
+            audioSource.PlayOneShot(sound2);
             playerScore+=1;
             // playerScoreText.text="Score:"+playerScore.ToString();
         }
